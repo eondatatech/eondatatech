9d587440688252a941c8cf7d75953a79059426df
shauryayellam <kushalreddy20@gmail.com>
2020-07-31 22:16:27 +0530

    Implemented application property base mechanism

--- EonDataProcessor/src/main/scala/com/eon/data/SparkJob.scala
+++ EonDataProcessor/src/main/scala/com/eon/data/SparkJob.scala
@@ -1,8 +1,13 @@
 package com.eon.data
 
+import java.util.Properties
+
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConverters._
+import scala.io.Source
 
 object SparkJob {
 
@@ -23,6 +28,30 @@ object SparkJob {
     spark
   }
 
+  def loadPropertyFile(classpathPropertyLocations: List[String]): Map[String, String] = {
+    var properties: Properties = new Properties()
+
+    classpathPropertyLocations.foreach(classpathPropertyLocation => {
+      val url = getClass.getResource(classpathPropertyLocation)
+      if (url != null) {
+        val source = Source.fromURL(url)
+        properties.load(source.bufferedReader())
+      } else {
+        throw new RuntimeException(s"Unable to load property file : `$classpathPropertyLocations` from classpath!!!")
+      }
+    })
+
+    // Convert java Properties to scala map
+    properties.asScala.toMap
+  }
+
+  def getProperties(): Map[String, String] = {
+    val propertyMap = this.loadPropertyFile(
+      List(s"local/eon/application.properties")
+    )
+    propertyMap
+  }
+
   def getSparkSession(): SparkSession = {
     if ((spark ne null) && !spark.sparkContext.isStopped) {
       return spark
@@ -36,7 +65,7 @@ object SparkJob {
     //create spark session
     println("============Create spark session=====-START")
     this.createSparkSession()
-    EonFactory.dataProcessor(args(0),args(1),args(2))
+    EonFactory.dataProcessor(args(0), args(1), args(2))
     println("============Create spark session=====-END")
   }
 }
